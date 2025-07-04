62058f1b8e761ad02909f4e40aaef9a2c61ce864
Tore I. Christoffersen <toreingolf@yahoo.no>
2025-07-04 14:03:21 +0200

    Add starter files

---
+++ src/main/java/net/toreingolf/emp_service/EmpServiceApplication.java
@@ -0,0 +1,13 @@
+package net.toreingolf.emp_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/toreingolf/emp_service/EmpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.toreingolf.emp_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
